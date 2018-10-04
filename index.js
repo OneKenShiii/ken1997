6069a33f18d9cb4bc3a9b05e5e1ed80cb6cc1fc7
OneKenShiii <apisitstar@gmail.com>
2018-10-04 12:15:33 +0700

    'Hello'

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log("Hello");
+const {execSync} = require('child_process');
+
+try {
+  console.log("Hello");
+  console.log(execSync('npm -g list --dept=0').toString());
+} catch (e) {
+  console.error(error);
+}
